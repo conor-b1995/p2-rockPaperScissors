de5adacbbd0e01658efac80e7b6935141da3a819
conor-b1995 <conor_carricktown@hotmail.com>
2022-06-17 21:02:58 +0000

    Add switch cases for every result option

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,71 @@ let paper = document.getElementById('paper');
 let scissors = document.getElementById('scissors');
 let lizard = document.getElementById('lizard');
 let spock = document.getElementById('spock');
-let userChoice = document.getElementById('user-choice')
-let compChoice = document.getElementById('comp-choice')
+let userChoice = document.getElementById('user-choice');
+let compChoice = document.getElementById('comp-choice');
+
+
+
+function youWin() {
+
+    console.log('You Win');
+  
+}
+
+function youLose() {
+
+    console.log('You Lose');
+
+}
+
+function draw() {
+
+    console.log('Its a Draw');
+    
+}
 
 
 function game(usersPick) {
+
     let computerChoice = randomComputerChoice();
-    
-    // console.log(usersPick);
-    // console.log(computerChoice);
+
+    switch (usersPick + computerChoice) {
+        case 'RockScissors':
+        case 'RockLizard':
+        case 'PaperRock':
+        case 'PaperSpock':
+        case 'ScissorsPaper':
+        case 'ScissorsLizard':
+        case 'LizardPaper':
+        case 'LizardSpock':
+        case 'SpockRock':
+        case 'SpockScissors':
+            youWin();
+            break;
+        case 'ScissorsRock':
+        case 'LizardRock':
+        case 'RockPaper':
+        case 'PaperSpock':
+        case 'PaperScissors':
+        case 'LizardScissors':
+        case 'PaperLizard':
+        case 'SpockLizard':
+        case 'RockSpock':
+        case 'ScissorsSpock':
+            youLose();
+            break;
+        case 'RockRock':
+        case 'PaperPaper':
+        case 'ScissorsScissors':
+        case 'LizardLizard':
+        case 'SpockSpock':
+            draw();
+            break;
+    }
+
     userChoice.innerHTML = usersPick;
     compChoice.innerHTML = computerChoice;
+
 }
 
 function randomComputerChoice() {
